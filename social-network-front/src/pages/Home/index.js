fdfcd2e6fe56793c8dc9cad8164f414cb546e41a
éde <edemarinho@outlook.com>
2021-09-21 18:45:12 -0300

    Set things up for material-ui implementation

--- social-network-front/src/pages/Home/index.js
+++ social-network-front/src/pages/Home/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from './components/Header/Header';
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
-import './style.css';
 
 function Home() {
     return (
